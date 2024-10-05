d492d71f2047d525d7d94502c429d9a37999d748
UtrOli <olepetnil@hotmail.com>
2024-10-05 17:55:07 +0200

    Minor styling changes

--- sketch.js
+++ sketch.js
@@ -13,32 +13,35 @@ let noiseScale = 0.1;
 
 function setup() {
   cvn = createCanvas(width, height, WEBGL);
-  ma = atan(cos(QUARTER_PI))+0.25;
+  ma = atan(cos(QUARTER_PI)) + 0.01;
   offX = random(10000);
   offY = random(10000);
 }
 
 function draw() {
-  background(color('#8bbabb'));
-  ortho(-viewDist,viewDist,viewDist,-viewDist,0,1300);
-  pointLight(255,255,255,0,0,400);
-  directionalLight(150,150,150,-0.8,-0.8,0);
+  background(color("#8bbabb"));
+  ortho(-viewDist, viewDist, viewDist, -viewDist, 0, 1300);
+  pointLight(255, 255, 255, 0, 0, 400);
+  directionalLight(150, 150, 150, -0.8, -0.8, 0);
   rotateX(ma);
   rotateY(-QUARTER_PI);
   normalMaterial();
-  for(let z=0;z<height;z+=size){
-    for(let x=0;x<width;x+=size){
+  for (let z = 0; z < height; z += size) {
+    for (let x = 0; x < width; x += size) {
       push();
-      let n = noise(offX+(x/size)*noiseScale,offY+(z/size)*noiseScale);
-      let h = floor(map(n,0,1,offset,max));
+      let n = noise(
+        offX + (x / size) * noiseScale,
+        offY + (z / size) * noiseScale
+      );
+      let h = floor(map(n, 0, 1, offset, max));
 
       //fill('rgba(50,150,50, 1)');
-      translate(x-width/2+90,h/2,z-height/2+90);
-      box(size,h,size);
+      translate(x - width / 2 + 90, h / 2, z - height / 2 + 90);
+      box(size, h, size);
       pop();
     }
   }
-  offX+=0.01;
+  offX += 0.01;
 }
 /*
 function mouseWheel(event) {
